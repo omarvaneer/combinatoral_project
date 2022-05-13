5618454e3a32f62cf8019e977c1992d6ac47a8ee
Alex Yeh <alexyeh33@gmail.com>
2022-05-13 12:58:48 -0400

    add timeout

--- exhaustiveSearch.py
+++ exhaustiveSearch.py
@@ -1,13 +1,17 @@
 import numpy as np
-#mxn array. M total elements in set, N subsets
+import multiprocessing
 
-#if we want to introduce randomness in exhaustive search order, I suggest adding a shuffle function to the dataset
+#The minimum set covering problem can be framed as an mxn array. M total elements in set (rows), N subsets (cols)
 
 class ExhaustiveSearch():
     def __init__(self,dataFile,k):
+
+        #load instance
         dataset = np.load(dataFile,allow_pickle=True)
         self.m = dataset['m']#total unique elements of set
         self.data = dataset['data']#array of subsets
+                                    # if we want to introduce randomness in exhaustive search order,
+                                    # I suggest adding a shuffle function to the dataset
         self.n = self.data.size #number of subsets
         self.k=int(np.min([k,self.data.size]))#number of subsets threshold
 
@@ -20,6 +24,7 @@ class ExhaustiveSearch():
         self.coolex_x = int(k)
         self.coolex_y = int(k)
 
+    #check a different combination
     def iterate(self):
         #check first
         if self.validateSets(self.subset_group):
@@ -30,10 +35,11 @@ class ExhaustiveSearch():
                 self.nextCombination()
                 return 0 #continue
             else:
+                #combination generator has exhausted all combinations
                 return -1 #terminate
 
 
-    #combination generation algorithm from 
+    #cool-lex combination generation algorithm from 
     #https://www.sciencedirect.com/science/article/pii/S0012365X07009570
     def nextCombination(self):
         self.coolex_b[self.coolex_x-1] = False
@@ -45,11 +51,11 @@ class ExhaustiveSearch():
         
         self.subset_group=np.nonzero(self.coolex_b)[0]
 
-#validation function
-#linearly look through all the subsets 
+    #validation function
+    #linearly look through all the subsets 
     def validateSets(self,subset_idxs):
         checkSet = np.zeros(self.m,dtype=bool)
-       
+
         #check all subsets
         for s in range(self.k):
             subset = self.data[subset_idxs[s]]
@@ -57,24 +63,42 @@ class ExhaustiveSearch():
                 checkSet[subset[i]] = True
 
         #logical AND across all elements
+        #if any of the elements in this array is zero, 
+        #then there is an element that is not covered by the subsets
         return np.all(checkSet)
 
         #could add more checks for early termination.
         #its more likely that the guess returns false/worst case where the alg must go through all the subsets anyways
         #so the early termination wouldn't be worth the extra overhead
 
-        
-#main event loop with timeout
-if __name__ == '__main__':
-    k=2
-    e = ExhaustiveSearch('example.npz',k)
-    result = e.iterate()
-
+#this runs the algorithm in a function that can be put in its own process
+def iterateExhaustiveSearch(e,result):
+    #exhaustive search loop
     while (result==0):
         result = e.iterate()
 
+    #algorithm termination (not timeout)
     if(result==1):
         print("minimum cover found")
         print(e.subset_group)
     else:
-        print("minimum subset not found")
+        print("no minimum cover")
+
+#main event loop with timeout
+if __name__ == '__main__':
+
+    #test data
+    k=2
+    e = ExhaustiveSearch('example.npz',k)
+
+    result = e.iterate()
+    p=multiprocessing.Process(target=iterateExhaustiveSearch,args=[e,result])
+    p.start()
+    p.join(1)#run for 1 second before timeout, use this to test algorithm termination case
+    #p.join(0.1)# use this to test timeout case
+
+    #check timeout
+    if p.is_alive():
+        p.terminate()
+        print("timeout - minimum cover not found")
+        
