0d51bb4569cbbc4c1915e68f8b70e3fc83e68b53
Alex Yeh <alexyeh33@gmail.com>
2022-05-16 15:42:46 -0400

    add k to example dataset

--- makeExample.py
+++ makeExample.py
@@ -1,6 +1,7 @@
 import numpy as np
 m=4
 n=5
+k=np.floor(n/3)
 #start counting from 0
 n1 = np.array([1,2],dtype=int)
 n2 = np.array([2],dtype=int)
@@ -12,4 +13,4 @@ data = np.array([n1,n2,n3,n4,n5],dtype=object)
 print(data)
 
 with open('example.npz', 'wb') as f0:
-    np.savez(f0,m=m,data=data)
+    np.savez(f0,m=m,k=k,data=data)
